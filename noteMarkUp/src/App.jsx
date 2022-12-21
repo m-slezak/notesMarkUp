257d1676eb927d82d0a84ed40244d01028641307
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-12-21 16:39:50 +0100

    Editor, Sidebar

---
+++ noteMarkUp/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <Editor />
+    </div>
+  )
+}
+
+export default App
