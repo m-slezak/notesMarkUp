68404ee677e6ab33ea4e4de24e50cfbc0842232b
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-12-21 17:03:31 +0100

    Spliter add

--- noteMarkUp/src/App.jsx
+++ noteMarkUp/src/App.jsx
@@ -1,13 +1,16 @@
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import Split from "react-split";
 
 function App() {
   return (
-    <div>
-      <Sidebar/>
-      <Editor />
-    </div>
-  )
+    <main>
+      <Split sizes={[30, 70]} direction="horizontal" className="split">
+        <Sidebar />
+        <Editor />
+      </Split>
+    </main>
+  );
 }
 
-export default App
+export default App;
