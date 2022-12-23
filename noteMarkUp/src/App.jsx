018cfc110afdf639e8a79c5fc6f9452726d81b97
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-12-23 05:55:35 +0100

    react mde

--- noteMarkUp/src/App.jsx
+++ noteMarkUp/src/App.jsx
@@ -3,6 +3,37 @@ import Editor from "./components/Editor";
 import Split from "react-split";
 
 function App() {
+  const [notes, setNotes] = React.useState([]);
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote;
+      })
+    );
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
   return (
     <main>
       <Split sizes={[30, 70]} direction="horizontal" className="split">
--- noteMarkUp/src/components/Sidebar.jsx
+++ noteMarkUp/src/components/Sidebar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Sidebar = () => {
-  return (
-    <div>
-      <h1>Sidebar</h1>
+const Sidebar = (props) => {
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`title ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className="text-snippet">Note {index + 1}</h4>
+      </div>
     </div>
-  )
-}
+  ));
+  return (
+    <section className="sidebar">
+      <div className="sidebar--top">
+        <h2>Notes</h2>
+        <button className="new-note" onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      <div></div>
+    </section>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
